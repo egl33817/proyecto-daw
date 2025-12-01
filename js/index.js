4bb8b73bf85835a9196b9fdb39696f468e292522
egl33817 <egl33817@educastur.es>
2025-12-01 23:30:34 +0100

    Se actualiza la página principal con la sección de ofertas y se crea un archivo JSON con los productos en oferta. Se crea también el fichero index.js, que contendrá las funciones que permitirán darle dinamismo a nuestra página principal. De momento hace la carga dinámica a partir del JSON antes citado de los datos de los productos en oferta, imágenes incluidas. Se crean los estilos pertinentes y se actualiza la gama de colores usada en nuestra web, también se incluye en .gitignore la nueva carpeta de imágenes de nuestros productos.

---
+++ js/index.js
@@ -0,0 +1,69 @@
+// Lectura de archivo JSON con los datos de los productos en oferta.
+let productosEnOferta = []
+
+fetch("./js/productosEnOferta.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data
+        // La primera vez que cargue la página, cargamos todos los productos.
+        cargarProductosEnOferta(productos)
+    })
+    .catch(error => console.log("Error: " + error))
+
+// Lectura de archivo JSON con los datos de los productos nuevos.
+/*let productosNuevos = []
+
+fetch("./js/productosNuevos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data
+        // La primera vez que cargue la página, cargamos todos los productos.
+        cargarProductosNuevos(productos)
+    })
+*/
+
+// Elementos del DOM.
+const contenedorOfertas = document.getElementById("contenedor-ofertas")
+//const seccionNovedades = document.getElementById("contenedor-novedades")
+
+// Cargamos los productos quee están en oferta.
+function cargarProductosEnOferta(listaProductosEnOferta) 
+{
+    contenedorOfertas.innerHTML = ""
+
+    listaProductosEnOferta.forEach(producto => {
+        const divProducto = document.createElement("div")
+        
+        divProducto.classList.add("producto")
+
+        divProducto.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}" />
+            <div class="producto-detalles">
+                <h3 class="producto-descripcion">${producto.descripcion}</h3>
+                <h4 class="producto-cantidad">${producto.cantidad}</h4>
+                <p class="producto-precio">
+                    <span class="producto-precio-anterior">${producto.precio} €</span> 
+                    <span class="producto-precio-actual">${calcularPrecioConDescuento(producto.precio, producto.descuento)} €</span>
+                </p>
+                <button class="producto-agregar" id="${producto.id}">Añadir al carrito</button>
+            </div>
+        `
+
+        contenedorOfertas.append(divProducto)
+    })
+
+    //actualizarListaDeBotonesAgregar();
+}
+
+function calcularPrecioConDescuento(precioOriginal, descuento)
+{
+    const rebaja = precioOriginal * (descuento / 100)
+
+    return (precioOriginal - rebaja).toFixed(2)
+}
+
+// Cargamos las novedades.
+function cargarProductosNuevos(listaProductosNuevos)
+{
+
+}
